3b276cfaf8da493ef9ff457be03e0bf59eae8326
Yash Maske patil <yashrajmaskepatil@gmail.com>
2026-07-21 08:59:00 +0530

    Add WebGLRenderer setup in script.js

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ window.innerWidth/window.innerHeight,
 0.1,
 1000
 );
+const renderer = new THREE.WebGLRenderer({
+canvas: document.getElementById("bg")
+});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
